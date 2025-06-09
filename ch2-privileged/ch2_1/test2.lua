af543978fdf5536de1f42d0d685b8d0936e9a79c
jason-fxz <jason341132@qq.com>
2025-06-09 11:12:48 +0800

    fix: fix 2.1 sysbench script
    Signed-off-by: jason-fxz <jason341132@qq.com>

--- ch2-privileged/ch2_1/test2.lua
+++ ch2-privileged/ch2_1/test2.lua
@@ -1,3 +1,10 @@
+-- This script is a sysbench test for a key-value store.
+
+-- load kv module
+local kv = require("kv")
+write_kv = kv.write_kv
+read_kv = kv.read_kv
+
 -- Define the range of keys
 local MAX_KEY = 2047
 function thread_init(thread_id)
@@ -11,14 +18,15 @@ function event(thread_id)
     if operation == 1 then
         -- Write Operation
         if write_kv(k, v) == -1 then
-            sb_report_error("Error writing key " .. k)
+            error("Error writing key " .. k)
         end
     else
         -- Read Operation
         local read_value = read_kv(k)
-        if read_value == -1 then
-            sb_report_error("Error reading key " .. k)
-        end
+        -- if read_value == -1 then
+        --     error("Error reading key " .. k)
+        -- end
     end
+end
 
--- sysbench --threads=100 --events=1000000 kv_test.lua run
+-- sysbench --threads=100 --events=1000000 test2.lua run
